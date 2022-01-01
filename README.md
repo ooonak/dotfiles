89363284848aec41952c187ea336637f5ac7ed5f
Casper Thorø Vium Pedersen <kanoo@posteo.de>
2022-01-01 12:24:29 +0100

    Updated documentation.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Vim setup
+Install dependencies: exuberant-ctags cscope
+
+Get this repo
+    $ git clone https://github.com/ooonak/dotfiles.git
+    $ cd ~/dotfiles
+    $ bash ./init.sh
+
+Now open vim and execute :PluginInstall
+
--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
+path=$(dirname $(realpath $0))
 
 mkdir -p $HOME/.vim/{bundle,plugin}
-ln -s ./vim/cscope_maps.vim $HOME/.vim/plugin/
-ln -s ./vim/vimrc $HOME/.vimrc
+ln -s ${path}/vim/cscope_maps.vim $HOME/.vim/plugin/
+ln -s ${path}/vim/vimrc $HOME/.vimrc
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
